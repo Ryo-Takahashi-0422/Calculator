503caeadf7203a7835306b3cdc64ef017309cab1
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2024-05-21 17:57:33 +0900

    1~9までのボタン押下時に桁数20未満なら数値が1桁目に入力されるようにした。また、ACボタン押下で0が入力されるようにした。

--- Calculator/ViewModels/ShellViewModel.cs
+++ Calculator/ViewModels/ShellViewModel.cs
@@ -8,19 +8,159 @@ namespace Calculator.ViewModels
 {
     public class ShellViewModel : Caliburn.Micro.Screen
     {
-		private int _value = 0;
+		private decimal _num = 0;
 
-		public int Value
+		public decimal Num
 		{
 			get 
 			{
-				return _value; 
+				return _num; 
 			}
 			set 
-			{ 
-				_value = Value;
+			{
+                _num = value;
+				NotifyOfPropertyChange(() => Num);
 			}
 		}
 
+		/// <summary>
+		/// [1]ボタンが入力された時の処理
+		/// </summary>
+		public void PushButtonOne()
+		{
+			string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+			{
+                Num = decimal.Parse(tempNum + "1");
+            }            
+        }
+
+        /// <summary>
+        /// [2]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonTwo()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "2");
+            }
+        }
+
+        /// <summary>
+        /// [3]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonThree()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "3");
+            }
+        }
+
+        /// <summary>
+        /// [4]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonFour()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "4");
+            }
+        }
+
+        /// <summary>
+        /// [5]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonFive()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "5");
+            }
+        }
+
+        /// <summary>
+        /// [6]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonSix()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "6");
+            }
+        }
+
+        /// <summary>
+        /// [7]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonSeven()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "7");
+            }
+        }
+
+        /// <summary>
+        /// [8]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonEight()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "8");
+            }
+        }
+
+        /// <summary>
+        /// [9]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonNine()
+        {
+            string tempNum = Num.ToString();
+
+            if (CheckCurrentNumLenght(tempNum))
+            {
+                Num = decimal.Parse(tempNum + "9");
+            }
+        }
+
+        /// <summary>
+        /// [AC]ボタンが入力された時の処理
+        /// </summary>
+        public void PushButtonAC()
+        {
+            Num = 0;
+        }
+
+        /// <summary>
+        /// 現在の入力値が20桁未満かチェックする。未満ならtrue、以上ならfalseを返す。
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private bool CheckCurrentNumLenght(string target)
+		{
+			if (target.Length < 20)
+			{
+				return true;
+			}
+
+			return false;
+		}
 	}
 }
